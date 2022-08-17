bf3983f0588e3a8339a8ce1d377fd2ec87725f94
Paul <paul@theapplegates.com>
2022-08-17 18:06:23 -0400

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 			wrap: true,
 		},
 	},
-	site: "https://www.astro-theme-cactus.netlify.app",
+	site: "https://cactus.paulapplegate.com",
 	integrations: [
 		mdx({}),
 		tailwind({
